d5f1d0cf9014316ff83b40f5d835ec9131d8717c
manojnambiraj <postboxmano@gmail.com>
2026-02-09 19:59:12 +0530

    JS Intro

---
+++ Javascript/Class1/script.js
@@ -0,0 +1,65 @@
+// Types of Programmings:
+
+    // Static programming
+        // C
+        // C++
+        // Java
+        // Php
+
+        // int a = 15000;
+
+    // Dynamic programming
+
+        // Python
+        // Javascript
+
+        // a = 100
+
+// Output Statements:
+
+    // alert()
+    // Console.log()
+    // DOM
+
+    // alert("hi")
+
+    // console.log("Hi");
+    
+// Version: ES6 (Ecma script)
+
+    // Old: ES5
+    // Old: ES6
+
+// Variables:
+
+    // myAgeInSchoolDays20 = 15
+
+    // console.log(myAgeInSchoolDays20);
+
+// Hoisting:
+
+    // var
+
+        // var a = 20
+
+        // var a = 25
+
+        // console.log(a);
+    
+
+    // let  -> ES6
+
+        // let a    // declare  // memory address
+
+        // a = 25   // definition
+
+        // a = 3500  // redefined
+
+        // console.log(a)
+        
+    // const  -> ES6
+
+        const a = 30  // declare  // memory address
+
+        console.log(a)
+        
