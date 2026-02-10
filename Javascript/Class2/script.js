efa1fe4017c4ef1bf2c0c3d6bf95023259b6d66d
manojnambiraj <postboxmano@gmail.com>
2026-02-10 19:46:03 +0530

    JS Datatypes and Operators

---
+++ Javascript/Class2/script.js
@@ -0,0 +1,118 @@
+// Variables
+
+    // var a = 10
+
+    // console.log(a);
+
+// Datatypes:
+
+    // let a = 100
+    // let a = "hello"
+    // let a = false
+    // let a
+    // const a = null
+
+    // console.log(a);
+    // console.log(typeof(a));
+
+// Operators:
+
+    // Arithmetic Operators
+        // (+, -, *, /, %, **, ++, --)
+
+            // let a = 5
+            // let b = 3
+            // let result
+
+            // result = a + b
+            // result = a - b
+            // result = a * b
+            // result = a / b
+            // result = a % b
+            // result = a ** b
+            // result =  ++a          // a++
+            // result = --a
+
+            // console.log(result);
+
+    // Assignment Operators
+        // (=, +=, -=, *=, /=, %=, **=)
+
+            // let a = 5
+
+            // a += 8    // a = a + 8
+            // a -= 8    // a = a - 8
+            // a *= 8    // a = a * 8
+            // a /= 8    // a = a / 8
+            // a %= 8    // a = a % 8
+            // a **= 8    // a = a ** 8
+
+            // console.log(a);
+
+    // Comparison Operators --> Boolean
+        // (==, ===, !=, !==, >, >=, <, <=)
+
+            // let age = 21
+            // let result
+
+            // result = (age == 18)
+            // result = (age === 18)
+            // result = (age != 18)
+            // result = (age !== 18)
+            // result = (age > 18)
+            // result = (age >= 18)
+            // result = (age < 18)
+
+            // console.log(result);
+
+    // Logical Operators
+        // (&&, ||, !)
+
+        // &&
+
+            // (true)  && (true)  -> true
+            // (true)  && (false) -> false
+            // (false) && (true)  -> false
+            // (false) && (false) -> false
+
+        // ||
+
+            // (true)  || (true)  -> true
+            // (true)  || (false) -> true
+            // (false) || (true)  -> true
+            // (false) || (false) -> false
+
+        // !
+
+            // true  --> false
+            // false --> true
+
+                // let age = 21
+                // let result
+
+                // result = (age > 18) && (age < 20)
+                // result = (age > 18) || (age < 30)
+
+                // console.log(!result);
+
+
+    // Ternary Operator  // ES6
+
+        // let age = 16
+        // let result
+
+        // result = (age > 18) ? "Eligible for voting" : "Not Eligible"
+
+        // console.log(result);
+
+    // Typeof Operator
+
+
+// Venila JS --> Core or fundamental
+
+// 2009 -> NodeJS
+
+// ReactJS
+// AngularJS
+// VueJS
+// NextJS
