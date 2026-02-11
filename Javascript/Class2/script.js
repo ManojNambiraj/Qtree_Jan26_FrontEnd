85910820c94535d083d1d5f2f0a63a2dc8d1aac2
manojnambiraj <postboxmano@gmail.com>
2026-02-11 19:43:06 +0530

    JS Operators Recall

--- Javascript/Class2/script.js
+++ Javascript/Class2/script.js
@@ -7,8 +7,8 @@
 // Datatypes:
 
     // let a = 100
-    // let a = "hello"
-    // let a = false
+    // let a = 'hello'
+    // let a = true
     // let a
     // const a = null
 
@@ -29,8 +29,8 @@
             // result = a * b
             // result = a / b
             // result = a % b
-            // result = a ** b
-            // result =  ++a          // a++
+            // result = a ** b        // Exponentiation
+            // result =  ++a             // a++
             // result = --a
 
             // console.log(result);
@@ -38,13 +38,13 @@
     // Assignment Operators
         // (=, +=, -=, *=, /=, %=, **=)
 
-            // let a = 5
+            // let a = 5            
 
-            // a += 8    // a = a + 8
+            // a += 8    // a = a + 3
             // a -= 8    // a = a - 8
             // a *= 8    // a = a * 8
             // a /= 8    // a = a / 8
-            // a %= 8    // a = a % 8
+            // a %= 8    // a = a % 8   // Modulus
             // a **= 8    // a = a ** 8
 
             // console.log(a);
@@ -52,7 +52,7 @@
     // Comparison Operators --> Boolean
         // (==, ===, !=, !==, >, >=, <, <=)
 
-            // let age = 21
+            // let age = "18"
             // let result
 
             // result = (age == 18)
@@ -90,15 +90,15 @@
                 // let age = 21
                 // let result
 
-                // result = (age > 18) && (age < 20)
-                // result = (age > 18) || (age < 30)
+                // result = (age > 18) && (age > 30)
+                // result = (age > 18) || (age < 20)
 
                 // console.log(!result);
 
 
     // Ternary Operator  // ES6
 
-        // let age = 16
+        // let age = 20
         // let result
 
         // result = (age > 18) ? "Eligible for voting" : "Not Eligible"
@@ -107,12 +107,19 @@
 
     // Typeof Operator
 
+// Control Statements:
 
-// Venila JS --> Core or fundamental
+    // Conditional Stmts
+        // if
+        // if else
+        // if else if
+        // switch case
 
-// 2009 -> NodeJS
+    // Looping Stmts
+        // for loop
+        // while loop
+        // do while
 
-// ReactJS
-// AngularJS
-// VueJS
-// NextJS
+    // Jumping stmts
+        // break
+        // continue
