f8a88970c5e09ba6888d9f9cbca112957b77c89e
manojnambiraj <postboxmano@gmail.com>
2026-02-13 19:56:23 +0530

    JS Array

---
+++ Javascript/Class4/script.js
@@ -0,0 +1,51 @@
+// Array:
+
+let a = [10, 40, 30, 20, 50, 60]
+//         #0  #1  #2  3   4   5    -> indexing
+
+console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+
+// a[1] = 2500
+
+// console.log(a);
+
+// console.log(a.length);
+// let temp = a.sort()
+// temp.reverse()
+
+// console.log(temp);
+
+// let fruits = ["apple", "banana", "orange", "grapes", "kiwi"]
+
+// console.log(fruits);
+// console.log(fruits.toString());
+// console.log(fruits.join(" * "));
+
+// fruits.pop()
+// fruits.push("watermelon")
+// console.log(fruits.shift());
+// fruits.unshift("watermelon")
+// fruits.splice(0, 2, "dragon")
+
+
+// console.log(fruits);
+
+
+// Object:
+
+let student1 = {name: "Raj", age: 23, dept: "IT", Mobile: 232323424234}
+
+console.log(student1.name);
+
+
+
+
+
+
+
+
+
+
+
