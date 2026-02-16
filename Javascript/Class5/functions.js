8e52c3a95c38d8435c3e9c0912126a0c43652999
manojnambiraj <postboxmano@gmail.com>
2026-02-16 19:27:14 +0530

    Js Arrays & Objects & Functions Basics

---
+++ Javascript/Class5/functions.js
@@ -0,0 +1,22 @@
+// Functions:
+
+    // Built-in function
+        // console.log("hi");
+        // alert("ji")
+        
+    // User defined function
+
+function demo(){            // definition
+    for(let i = 0; i <= 10; i++){
+        if(i == 6){
+            break
+        }
+        console.log(i);
+    }
+}
+
+demo()
+console.log("Good morning");
+demo()
+console.log("Good afternoon");
+demo()
---
+++ Javascript/Class5/script.js
@@ -0,0 +1,65 @@
+// Arrays:
+
+    // let a = [10, 20, 30, 40, 50, true, "hi"]
+    // //       0   1   2   3  ................
+
+    // console.log(a);
+    // console.log(a[1]);
+
+    // a[2] = "Good"
+
+    // console.log(a);
+
+    // console.log(a.length);
+
+// Objects
+
+    // let student = {
+    //     name: "Ravi", 
+    //     age: 23, 
+    //     dept: "EEE", 
+    //     mobile: 9876543222,
+    //     isMarried: false,
+    //     address: {
+    //         dno: 2,
+    //         street: "xyz"
+    //     },
+    //     favColor: ["black", "brown", "blue"]
+    // }
+
+    // console.log(student);
+    // console.log(student.name);
+    // console.log(student.dept);
+    // console.log(student.favColor[0]);
+
+// Destructuring: (ES6)
+
+    // Array destructure
+
+        // let fruits = ["apple", "banana", "orange", "grapes"]
+                        //    0      1         2
+        // const [red, yellow, orange, violet] = fruits
+
+        // console.log(red);
+        // console.log(yellow);
+
+    // Object destructure
+
+        // let student = {
+        //     name: "Ravi", 
+        //     age: 23, 
+        //     dept: "EEE", 
+        //     mobile: 9876543222
+        // }
+
+        // const {name, age, mobile} = student
+
+        // console.log(name);
+        // console.log(age)
+        // console.log(mobile);
+    
+    
+    
+
+
+
