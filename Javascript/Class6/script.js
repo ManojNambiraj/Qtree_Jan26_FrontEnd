d9f070bdadb05ab9e566df585a140a9f9dad205a
manojnambiraj <postboxmano@gmail.com>
2026-02-17 19:36:37 +0530

    JS Functions

---
+++ Javascript/Class6/script.js
@@ -0,0 +1,67 @@
+// Function
+
+    // Basic
+
+        // function demo(){
+        //     console.log("Hi everyone");   
+        // }
+
+        // demo()
+
+// Normal Function --> ES5
+
+    // function demo(a, b){
+    //     console.log(a);
+    // }
+
+
+    // demo([10, 11, 12, 13, 14], 2)
+    // demo(50, 4)
+    // demo(7, 3)
+    // demo(4, 2)
+    // demo(67, 2)
+
+    // function demo(a, b){
+    //     return a * b
+    // }
+
+    // let result = demo(2, 5)
+
+    // console.log(result);
+
+
+// Ananymous Function
+
+    // Ex: 1
+
+        // const demo = function(){
+        //     console.log("Hello world");
+        // }
+
+        // demo()
+
+    // Ex: 2
+
+        // let stu = {
+        //     name: "Savitha",
+        //     age: 29,
+        //     isGraduated: function(){
+        //         return true
+        //     }
+        // }
+
+        // console.log(stu.name);
+        // console.log(stu.isGraduated());
+
+// Arrow Function: (ES6)
+
+    // const demo = (a, b, c) => a * b + c
+
+    // console.log(demo(5, 2, 9));
+
+// IIFE function: (Immediately Invoked Function Expression)
+
+    // (() => {
+    //     console.log("hello");
+    // })() 
+    
