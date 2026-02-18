233149f49c38322bcb998d03e035b55e6974d5fb
manojnambiraj <postboxmano@gmail.com>
2026-02-18 19:37:24 +0530

    JS MRF Methods

---
+++ Javascript/Class7/script.js
@@ -0,0 +1,40 @@
+// Methods: -> built-in function
+
+    // Array Methods: (ES6) (MRF)
+        // Map
+        // Reduce
+        // Filter
+
+// let nums = [2, 9, 7, 22, 45, 66, 91, 88]
+//          0  1  2   3..............7
+// console.log(nums.length);
+// nums.pop()
+// nums.push(100)
+// nums.shift()
+// nums.unshift(200)
+
+// for(let i = 0; i < nums.length; i++){
+//     console.log(nums[i]);
+// }
+
+// Map:
+
+    // const result = nums.map((item) => {
+    //   return item
+    // })
+
+    // console.log(result);
+
+// Filter
+
+    // let nums = [2, 9, 7, 22, 45, 66, 91, 88]
+
+    // const result = nums.filter((item, index) => index >= 2)
+
+    // console.log(result);
+
+// Reduce
+
+    // let result = nums.reduce((acc, curr) => (acc + curr)/nums.length)
+
+    // console.log(result);
