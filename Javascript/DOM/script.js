85b83eb86616f04e907cd2ebe1f12055015e81f9
manojnambiraj <postboxmano@gmail.com>
2026-02-24 19:51:07 +0530

    DOM Class1

---
+++ Javascript/DOM/script.js
@@ -0,0 +1,39 @@
+// DOM -->  Document Object Model
+
+    // window.alert("hi")
+
+    // CRUD -> Create Read Update Delete
+
+// let ele = document.getElementById("demo")
+
+// console.log(ele);
+
+// ele.innerHTML = "<a href='next.html'>Next</a>"
+// ele.innerText = "Hello world"
+// ele.style.color = "Red"
+// ele.style.backgroundColor = "aqua"
+
+// let ele1 = document.getElementsByClassName("para")
+
+// console.log(ele1);
+
+// ele1[0].innerText = "Hello"
+
+// ele1[1].innerText = "World"
+
+// let ele2 = document.getElementsByTagName("p")
+
+// console.log(ele2);
+
+// ele2[1].innerText = "Hello"
+
+// let ele3 = document.getElementById("one").value = ""
+
+// console.log(ele3);
+
+
+let newele = document.createElement("h1")
+
+newele.innerText = "Good evening"
+
+document.body.append(newele)
